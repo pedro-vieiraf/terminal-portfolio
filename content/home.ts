a54bf030a9ff36e664cc0286ff500331164711b3
Pedro Vieira <pedrovieirafdev@gmail.com>
2025-12-19 15:06:49 -0300

    removing capital I

--- content/home.ts
+++ content/home.ts
@@ -1,10 +1,10 @@
 export const homeLines = [
   { prefix: 'system', text: 'pedro has joined the chat' },
-  { prefix: 'pedro', text: "hello, I'm Pedro, backend developer" },
+  { prefix: 'pedro', text: "hello, i'm Pedro, a backend developer" },
   { prefix: 'pedro', text: "coffee-powered and code-fueled!"},
-  { prefix: 'pedro', text: "I build backend systems and work with APIs and AI integration" },
+  { prefix: 'pedro', text: "i build backend systems and work with APIs and AI integration" },
   { prefix: 'pedro', text: "feel free to explore my projects and skills!" },
-  { prefix: 'pedro', text: "sometimes I talk to computers more than humans…" },
-  { prefix: 'pedro', text: "but I promise I'm approachable!" },
+  { prefix: 'pedro', text: "sometimes i talk to computers more than humans…" },
+  { prefix: 'pedro', text: "but i promise i'm approachable!" },
   { prefix: 'pedro', text: "{\\__/}\n( •-•)\n/つ☕" },
 ];
